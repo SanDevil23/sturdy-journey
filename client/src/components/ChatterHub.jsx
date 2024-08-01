7655b411e0f7a62c328b5cb55c7d84ad8f3e3f4d
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2024-08-01 19:09:53 +0530

    fixed error

--- client/src/components/ChatterHub.jsx
+++ client/src/components/ChatterHub.jsx
@@ -8,9 +8,7 @@ const ChatterHub = () => {
                 <p className='mt-4 md:mt-8 lg:mt-12 text-left text-2xl font-extralight text-slate-400'>CHAT APPLICATION</p>
                 <p className='mt-4 md:mt-8 lg:mt-12 text-left text-6xl font-semibold'>ChatterHub</p>
                 <p className='mt-4 md:mt-8 lg:mt-12 text-left text-xl font-medium'>Real Time One-to-One Messaging Application</p>
-
                 <p className='mt-4 md:mt-8 lg:mt-12 text-left text-lg font-medium'><b>Built With : </b>ReactJS, Node, ExpressJS, MongoDB (NoSQL Database, Cloud Functions, Cloud Storage),Git, ChakraUI, Cloudinary. </p>
-                
                 <br />
                 <button className='flex p-4 w-fit h-1 items-center border-solid border border-white'><a href="https://www.github.com/SanDevil23/ChatterHub">View the Code</a></button>
             </div>
