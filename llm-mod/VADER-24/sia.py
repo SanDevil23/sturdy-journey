87bc2cdcffff163a18f75f1e4efbb75d30b79f75
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2024-10-23 14:29:28 +0530

    logs reduced and avg scored provided in SIA -ref Issue #2

--- llm-mod/VADER-24/sia.py
+++ llm-mod/VADER-24/sia.py
@@ -1,6 +1,36 @@
+import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import seaborn as sns
+
+plt.style.use('ggplot')
 
 # object for SentimentIntensityAnalyzer
 sia = SentimentIntensityAnalyzer()
 
-print(sia.polarity_scores("This is the worst thing ever"))
+df = pd.read_csv("./llm-mod/VADER-24/Reviews.csv")
+
+df = df.head(1500)
+
+# calculating the avg of the 500 records
+pos = 0
+neg = 0
+neu = 0 
+
+for i in range(1500):
+    text = df['Text'].values[i]
+    score = sia.polarity_scores(text)
+    # print(score)
+    pos+= score['pos']
+    neg+= score['neg']
+    neu+= score['neu']
+
+
+print("FINAL DATA")
+print("positive sentiments  = ", float(pos/500))
+print("neutral sentiments  = ", float(neu/500))
+print("negative sentiments  = ", float(neg/500))
+
+
